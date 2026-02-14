581d8554bdeb48d825ef37eae02341e0f4a610d5
Kevin Ahrendt <kahrendt@gmail.com>
2026-02-14 10:29:11 -0500

    fix issue where log_cpu_usage: false would fail to build

--- esphome/components/debug/debug_esp32.cpp
+++ esphome/components/debug/debug_esp32.cpp
@@ -15,10 +15,12 @@
 #include <Esp.h>
 #endif
 
+#include "sdkconfig.h"
+
+#if CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
-#include "sdkconfig.h"
 
 static const UBaseType_t STATS_TASK_PRIORIY = 3;
 static const uint32_t STATS_DELAY_MS = 5000;
@@ -132,6 +134,7 @@ static void stats_task(void *arg) {
     }
   }
 }
+#endif  // CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS
 
 namespace esphome {
 namespace debug {
@@ -231,9 +234,11 @@ const char *DebugComponent::get_wakeup_cause_(std::span<char, RESET_REASON_BUFFE
 }
 
 void DebugComponent::setup() {
+#if CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS
   if (this->log_cpu_usage_) {
     xTaskCreate(stats_task, "stats", 4096, nullptr, STATS_TASK_PRIORIY, nullptr);
   }
+#endif  // CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS
 }
 
 void DebugComponent::log_partition_info_() {
